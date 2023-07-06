232792bf5002ab302bd9bd12b629d79749758a55
ShecktorS <ettoresanfilippo@gmail.com>
2023-07-06 19:02:03 +0200

    bootstrap improvements

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe(
+      'Ricetta di prova',
+      'Una bella ricetta',
+      'https://th.bing.com/th/id/OIP.QExi6APdpquqTSgicOJpWgHaE-?pid=ImgDet&rs=1'
+    ),
+  ];
 
   constructor() {}
 
